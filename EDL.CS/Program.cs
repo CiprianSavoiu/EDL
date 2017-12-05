2ed8a1317c38693cc8feb285e4e4d2704d158492
Ciprian Savoiu <ciprian.savoiu@nmgroup.com>
2017-12-05 23:55:49 +0000

    lambdas

--- EDL.CS/Program.cs
+++ EDL.CS/Program.cs
@@ -1,16 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace EDL.CS
 {
 
+    public delegate int BizRuleDelegate(int x, int y);
+
+
     class Program
     {
         static void Main(string[] args)
         {
+
+            var custs = new List<Customer>
+            {
+                new Customer
+                {
+                    City = "Phoenix",
+                    FirstName = "Jon",
+                    LastName = "Doe",
+                    Id = 1
+                },
+                new Customer
+                {
+                    City = "Phoenix",
+                    FirstName = "Jane",
+                    LastName = "Doe",
+                    Id = 500
+                },
+                new Customer
+                {
+                    City = "Seatle",
+                    FirstName = "Suki",
+                    LastName = "Pizzoro",
+                    Id = 3
+                },
+                new Customer
+                {
+                    City = "New York City",
+                    FirstName = "Michelle",
+                    LastName = "SMith",
+                    Id = 4
+                }
+            };
+
+
+            var phxCust = custs
+                .Where(c => c.City == "Phoenix" && c.Id <100)
+                .OrderBy(c => c.FirstName);
+
+            foreach (var cust in phxCust)
+            {
+                Console.WriteLine(cust.FirstName);
+            }
+
+
+            var data = new ProcessData();
+
+            BizRuleDelegate addDelegate = (x, y) => x + y;
+            BizRuleDelegate multiplyDelegate = (x, y) => x * y;
+
+            data.Process(2, 3, multiplyDelegate);
+
+            Func<int, int, int> funcAddDel = (x, y) => x + y;
+            Func<int, int, int> funcMultiplyDel = (x, y) => x * y;
+
+            data.ProcessFunct(2, 5, funcAddDel);
+
+            Action<int, int> myAddAction = (x, y) => Console.WriteLine(x + y);
+            Action<int, int> myMultiplyAction = (x, y) => Console.WriteLine(x * y);
+
+            data.ProcessAction(2, 3, myAddAction);
+
+
             //var del1 = new WorkPerformedHandler(WorkPerformed1);
             //var del2 = new WorkPerformedHandler(WorkPerformed2);
             //var del3 = new WorkPerformedHandler(WorkPerformed3);
@@ -20,8 +87,13 @@ namespace EDL.CS
             //var finalHours = del1(10, WorkType.GenerateReports);
 
             var worker = new Worker();
-            worker.WorkPerformed += new EventHandler<WorkPerformedEventArgs>(Worker_WorkPerformed);
-            worker.WorkCompleted += new EventHandler(Worker_WorkCompleted);
+            worker.WorkPerformed += (s, e) =>
+            {
+                Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+                Console.WriteLine("SSS");
+            };
+            worker.WorkCompleted += (s, e) => Console.WriteLine("Worker is done");
+            
             worker.DoWork(8, WorkType.GenerateReports);
 
 
@@ -30,15 +102,15 @@ namespace EDL.CS
 
         }
 
-        static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
-        {
-            Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
-        }
+        //private static void Worker_WorkPerformed(object sender, WorkPerformedEventArgs e)
+        //{
+        //    Console.WriteLine("Hours worked: " + e.Hours + " " + e.WorkType);
+        //}
 
-        static void Worker_WorkCompleted(object sender, EventArgs e)
-        {
-            Console.WriteLine("Worker is done");
-        }
+        //static void Worker_WorkCompleted(object sender, EventArgs e)
+        //{
+        //    Console.WriteLine("Worker is done");
+        //}
 
         //static void DoWork(WorkPerformedHandler del)
         //{
